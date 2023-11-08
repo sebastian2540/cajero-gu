b8736fb0edcfde524f04cedd87eb2656cb5baf3a
sebastian2540 <sebastian.villada.jaramillo@gmail.com>
2023-11-07 21:38:15 -0500

    Se continua con el login

--- assets/controller/ControllerLogin.js
+++ assets/controller/ControllerLogin.js
@@ -1,38 +1,161 @@
-import { UserModel } from '../model/ModelLogin.js';
-console.table(UserModel.users)
+import { usuarios } from "../model/ModelLogin.js";
+console.log(usuarios);
 
-document.addEventListener('DOMContentLoaded', () => {
-    const usernameInput = document.getElementById('username');
-    const passwordInput = document.getElementById('password');
-    const btnIniciar = document.getElementById('login-button');
+let inputs = document.querySelectorAll('.controls')
+inputs.forEach(function (input) {
+    input.addEventListener('keyup', validarFormulario)
+})
+
+// Validaciones de Expresiciones Regulares
+let username = /^[a-zA-ZÀ-ÿ\s]{1,40}$/
+let password = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm
+
+function validarFormulario(e) {
+    console.log(e)
+    switch (e.target.name) {
+        case 'username':
+            if (username.test(e.target.value)) {
+                document.getElementById('username').style.backgroundColor = 'rgb(176, 204, 176)'
+            }
+            break;
+
+        case 'password':
+            if (password.test(e.target.value)) {
+                document.getElementById('password').style.backgroundColor = 'rgb(176, 204, 176)'
+            } else {
+                document.getElementById('password').style.backgroundColor = 'rgb(204, 176, 176)'
+            }
+            break;
+
+        default:
+            break;
+    }
+}
+
+// Inicio de Sesión
+let loginButton = document.getElementById('loginButton')
+loginButton.addEventListener('click', function () {
+
+    let username = document.getElementById('username').value
+    let password = document.getElementById('password').value
     const message = document.getElementById('message');
+    let intentos = 5
 
-    btnIniciar.addEventListener('click', () => {
-        const username = usernameInput.value;
-        const password = passwordInput.value;
+    while (intentos > 0) {
+        //Validación del Usuario 1
+        if (username == 'Administrador001') {
+            if (password == 'Administrador.001') {
+                console.log('Inicio de sesión exitoso. ' + username)
 
-        const usernameRegex = usernameInput.value;
-        const passwordRegex = passwordInput.value;
+                //Configuración de alerta
+                const AlertConfig = document.getElementById('liveToastGuardar')
+                const toastGuardar = bootstrap.Toast.getOrCreateInstance(AlertConfig)
+                toastGuardar.show()
 
-        const userRegex = UserModel.authenticateRegex(usernameRegex, passwordRegex);
+                //Control de ventanas 
+                setTimeout(() => {
+                    document.getElementById('login').style.display = 'none'
+                    document.getElementById('panel').style.display = 'block'
+                }, 3000);
+                break;
+                
+            } else {
+                intentos--
+                if (intentos > 0) {
+                    console.log('Datos incorrectos. Te quedan ' + intentos)
+                    console.log('Acceso denegado')
 
-        if (userRegex) {
-            message.textContent = 'Usuario o contraseña no cumplen con los requisitos';
-        } else {
-            message.textContent = 'Usuario o contraseña no cumplen con los requisitos';
+                    //Configuración de alerta
+                    const AlertConfig = document.getElementById('liveToastCancelar')
+                    const toastCancelar = bootstrap.Toast.getOrCreateInstance(AlertConfig)
+                    toastCancelar.show()
+                } else {
+                    console.log('Has excedido el número máximo de intentos. Cuenta bloqueda.')
+                    console.log('Acceso denegado')
+                    const AlertConfig = document.getElementById('liveToastCancelarBloqueda')
+                    const toastCancelarBloqueda = bootstrap.Toast.getOrCreateInstance(AlertConfig)
+                    toastCancelarBloqueda.show()
+                }
+
+            }
         }
+    }
+})
 
-        // if (!UserModel.usernameRegex.test(username) || !UserModel.passwordRegex.test(password)) {
-        //     message.textContent = 'Usuario o contraseña no cumplen con los requisitos';
-        //     return;
-        // }
 
-        const user = UserModel.authenticate(username, password);
 
-        if (user) {
-            message.textContent = 'Inicio de sesión exitoso';
-        } else {
-            message.textContent = 'Usuario o contraseña invalido';
-        }
-    });
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { UserModel } from '../model/ModelLogin.js';
+// console.table(UserModel.users)
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     const usernameInput = document.getElementById('username');
+//     const passwordInput = document.getElementById('password');
+//     const btnIniciar = document.getElementById('login-button');
+//     const message = document.getElementById('message');
+
+//     btnIniciar.addEventListener('click', () => {
+//         const username = usernameInput.value;
+//         const password = passwordInput.value;
+
+//         const usernameRegex = usernameInput.value;
+//         const passwordRegex = passwordInput.value;
+
+//         const userRegex = UserModel.authenticateRegex(usernameRegex, passwordRegex);
+
+//         if (userRegex) {
+//             message.textContent = 'Usuario o contraseña no cumplen con los requisitos';
+//         } else {
+//             message.textContent = 'Usuario o contraseña no cumplen con los requisitos';
+//         }
+
+//         // if (!UserModel.usernameRegex.test(username) || !UserModel.passwordRegex.test(password)) {
+//         //     message.textContent = 'Usuario o contraseña no cumplen con los requisitos';
+//         //     return;
+//         // }
+
+//         const user = UserModel.authenticate(username, password);
+
+//         if (user) {
+//             message.textContent = 'Inicio de sesión exitoso';
+//         } else {
+//             message.textContent = 'Usuario o contraseña invalido';
+//         }
+//     });
+// });
--- assets/model/ModelLogin.js
+++ assets/model/ModelLogin.js
@@ -1,20 +1,46 @@
-export const UserModel = {
-    users: [
-        { username: "Administrador4585", password: "Admon123*" },
-        { username: "Cajero6933", password: "Cajero123*" },
-    ],
-
-    usernameRegex: /[a-zA-Z][a-zA-Z0-9-_]{3,32}/gi,
-    passwordRegex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm,
-
-    authenticateRegex(usernameRegex, passwordRegex){
-        if (!this.usernameRegex.test(username) || !this.passwordRegex.test(password)) {
-            return null;
-        }
-        return this.usernameRegex.find((userRegex) => userRegex.usernameRegex === usernameRegex && userRegex.passwordRegex === passwordRegex);
+export let usuarios = [
+    {
+        usuario: 'Administrador001',
+        contrasena: 'Administrador.001',
+        id: 1
     },
 
-    authenticate(username, password) {
-        return this.users.find((user) => user.username === username && user.password === password);
+    {
+        usuario: 'Administrador002',
+        contrasena: 'Administrador.002',
+        id: 2
     },
-};
+
+    {
+        usuario: 'Cajero001',
+        contrasena: 'Cajero.001',
+        id: 3
+    },
+
+    {
+        usuario: 'Cajero002',
+        contrasena: 'Cajero.002',
+        id: 4
+    },
+]
+
+// export const UserModel = {
+//     users: [
+//         { username: "Administrador4585", password: "Admon123*" },
+//         { username: "Cajero6933", password: "Cajero123*" },
+//     ],
+
+//     usernameRegex: /[a-zA-Z][a-zA-Z0-9-_]{3,32}/gi,
+//     passwordRegex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm,
+
+//     authenticateRegex(usernameRegex, passwordRegex){
+//         if (!this.usernameRegex.test(username) || !this.passwordRegex.test(password)) {
+//             return null;
+//         }
+//         return this.usernameRegex.find((userRegex) => userRegex.usernameRegex === usernameRegex && userRegex.passwordRegex === passwordRegex);
+//     },
+
+//     authenticate(username, password) {
+//         return this.users.find((user) => user.username === username && user.password === password);
+//     },
+// };
